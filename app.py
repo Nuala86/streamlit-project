4569eba6d7ad8a9e0a144f84b8113277c790e174
Nuala86 <katharina.stipp@gmail.com>
2025-03-07 14:43:29 +0100

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,12 @@ import streamlit as st
 
 st.title("Happiness 🙂")
 
+st.markdown("<h3 style='color: #6E66CC;'>A single factor or the interplay of many?</h3>", unsafe_allow_html=True)
+
+st.markdown("""
+
+How happy is our society – and is happiness measurable? Various indicators make it possible to describe the subjective and objective well-being of a society or an individual. But which country is truly the happiest? And can we identify differences in terms of prosperity, social environment, or political conditions? Is happiness determined by individual factors alone, or is it ultimately the interplay of several influences?
+
+
+---
+""")
